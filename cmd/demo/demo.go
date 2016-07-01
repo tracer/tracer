dca9564ad61489b5dc2a5c4ebdf6274db35250b4
Dominik Honnef <dominik@honnef.co>
2016-07-01 02:41:29 +0200

    Adjust demo to showcase Zipkin-style RPC tracing

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -15,18 +15,59 @@ func main() {
 	opentracing.InitGlobalTracer(tracer.NewTracer(storage, tracer.RandomID{}))
 	tracer := opentracing.GlobalTracer()
 
-	span1 := tracer.StartSpan("frontend")
-	span1.SetTag("url", "/hello")
-	span1.SetTag("user_id", 123)
-	span2 := opentracing.StartChildSpan(span1, "backend")
-	span2.SetTag("instance", 456)
-	span3 := opentracing.StartChildSpan(span2, "mysql")
-	span3.LogEventWithPayload("prepare", "SELECT hello FROM world")
-	span3.LogEvent("execute")
-	span3.Finish()
-	span4 := opentracing.StartChildSpan(span2, "redis")
-	span4.LogEvent("store")
+	var carrier opentracing.HTTPHeaderTextMapCarrier
+
+	/* <service1> */
+	span1 := tracer.StartSpan("service1")
+	span2 := opentracing.StartChildSpan(span1, "rpc-req-service2")
+	span2.LogEvent("cs")
+	carrier = opentracing.HTTPHeaderTextMapCarrier{}
+	tracer.Inject(span2, opentracing.TextMap, carrier)
+
+	/* <service2> */
+	span3, _ := tracer.Join("rpc-req-service2", opentracing.TextMap, carrier)
+	span3.LogEvent("sr")
+	span4 := opentracing.StartChildSpan(span3, "service2")
+	span5 := opentracing.StartChildSpan(span4, "rpc-req-service3")
+	span5.LogEvent("cs")
+	carrier = opentracing.HTTPHeaderTextMapCarrier{}
+	tracer.Inject(span5, opentracing.TextMap, carrier)
+
+	/* <service3> */
+	span6, _ := tracer.Join("rpc-req-service3", opentracing.TextMap, carrier)
+	span6.LogEvent("sr")
+	span7 := opentracing.StartChildSpan(span6, "service3")
+	span7.Finish()
+	span6.LogEvent("ss")
+	span6.Finish()
+	// Sen2 result to service2
+	/* </service3> */
+	span5.LogEvent("cr")
+	span5.Finish()
+
+	span8 := opentracing.StartChildSpan(span4, "rpc-req-service4")
+	span8.LogEvent("cs")
+	carrier = opentracing.HTTPHeaderTextMapCarrier{}
+	tracer.Inject(span8, opentracing.TextMap, carrier)
+
+	/* <service4> */
+	span9, _ := tracer.Join("rpc-req-service4", opentracing.TextMap, carrier)
+	span9.LogEvent("sr")
+	span10 := opentracing.StartChildSpan(span9, "service4")
+	span10.Finish()
+	span9.LogEvent("ss")
+	span9.Finish()
+	// Send result to service2
+	/* </service4> */
+	span8.LogEvent("cr")
+	span8.Finish()
+
 	span4.Finish()
+	span3.LogEvent("ss")
+	span3.Finish()
+	/* </service2> */
+	span2.LogEvent("cr")
 	span2.Finish()
 	span1.Finish()
+	/* </service1> */
 }
