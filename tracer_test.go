699426153f089b3d2ec83a79af0adcfd4e265305
Dominik Honnef <dominik@honnef.co>
2016-07-03 10:00:04 +0200

    Add test for text carrier

--- tracer_test.go
+++ tracer_test.go
@@ -3,7 +3,39 @@ package tracer
 import (
 	"bytes"
 	"testing"
+
+	"github.com/opentracing/opentracing-go"
 )
+
+func TestText(t *testing.T) {
+	sp := &Span{
+		RawSpan: RawSpan{
+			SpanID:   1,
+			ParentID: 2,
+			TraceID:  3,
+			Baggage: map[string]string{
+				"k1": "v1",
+				"k2": "",
+			},
+		},
+	}
+
+	carrier := opentracing.TextMapCarrier{}
+	if err := textInjecter(sp, carrier); err != nil {
+		t.Fatal("unexpected error: ", err)
+	}
+	traceID, parentID, spanID, baggage, err := textJoiner(carrier)
+	if err != nil {
+		t.Fatal("unexpected error: ", err)
+	}
+	if traceID != sp.TraceID || parentID != sp.ParentID || spanID != sp.SpanID ||
+		len(baggage) != 2 || baggage["k1"] != "v1" || baggage["k2"] != "" {
+
+		t.Errorf("got (%d, %d, %d, %v), want (%d, %d, %d, %v)",
+			traceID, parentID, spanID, baggage,
+			sp.TraceID, sp.ParentID, sp.SpanID, sp.Baggage)
+	}
+}
 
 func TestBinary(t *testing.T) {
 	sp := &Span{
