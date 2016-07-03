4ba226ccdd90d076bf70862936e11a08e3298c6a
Dominik Honnef <dominik@honnef.co>
2016-07-03 04:33:42 +0200

    Check SQL schema into repository

---
+++ storage/postgres/schema.sql
@@ -0,0 +1,36 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE spans (
+       id bigint PRIMARY KEY,
+       trace_id bigint,
+       time tstzrange NOT NULL,
+       operation_name text NOT NULL
+);
+
+CREATE INDEX idx_spans_trace_id ON spans (trace_id);
+CREATE INDEX idx_spans_time ON spans USING gist (time);
+CREATE INDEX idx_spans_operation_name ON spans (operation_name);
+
+CREATE TABLE tags (
+       id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+       trace_id bigint NOT NULL REFERENCES spans ON DELETE CASCADE,
+       span_id bigint NOT NULL REFERENCES spans ON DELETE CASCADE,
+       key text NOT NULL,
+       value text NOT NULL,
+       time timestamp with time zone NULL
+);
+
+CREATE INDEX idx_tags_span_id ON tags (span_id);
+CREATE INDEX idx_tags_key_value ON tags (key, value);
+
+CREATE TYPE relation AS ENUM ('parent');
+
+CREATE TABLE relations (
+       id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+       span1_id bigint NOT NULL REFERENCES spans ON DELETE CASCADE,
+       span2_id bigint NOT NULL REFERENCES spans ON DELETE CASCADE,
+       kind relation NOT NULL
+);
+
+CREATE INDEX idx_relations_span1_id ON relations (span1_id);
+CREATE INDEX idx_relations_span2_id ON relations (span2_id);
