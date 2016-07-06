238e756d822be4603dcdcd9ced4c77917b80c943
Dominik Honnef <dominik@honnef.co>
2016-07-06 08:47:13 +0200

    Extend schema in preparation of aggregate view feature

--- storage/postgres/schema.sql
+++ storage/postgres/schema.sql
@@ -41,3 +41,17 @@ CREATE TABLE relations (
 
 CREATE INDEX idx_relations_span1_id ON relations (span1_id);
 CREATE INDEX idx_relations_span2_id ON relations (span2_id);
+
+CREATE MATERIALIZED VIEW dependencies (name1, name2, count) AS
+SELECT s1.service_name, s2.service_name, COUNT(*)
+FROM
+  spans AS s1
+    JOIN tags AS t ON t.span_id = s1.id
+    JOIN relations AS r ON r.span1_id = s1.id
+    JOIN spans AS s2 ON r.span2_id = s2.id
+WHERE
+  r.kind = 'parent' AND
+  t.key = 'span.kind' AND
+  t.value = 'client'
+GROUP BY
+  s1.service_name, s2.service_name;
