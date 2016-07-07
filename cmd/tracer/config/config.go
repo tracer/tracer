901ca1ec120c367da2ee67748fc964ca7d585ccf
Dominik Honnef <dominik@honnef.co>
2016-07-07 11:10:01 +0200

    Add first pieces of configuration system

---
+++ cmd/tracer/config/config.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/BurntSushi/toml"
+)
+
+type MissingSectionError string
+
+func (err MissingSectionError) Error() string {
+	return fmt.Sprintf("missing configuration section: %s", string(err))
+}
+
+type MissingKeyError string
+
+func (err MissingKeyError) Error() string {
+	return fmt.Sprintf("missing configuration key: %s", string(err))
+}
+
+type WrongValueTypeError struct {
+	Key  string
+	Type string
+}
+
+func (err WrongValueTypeError) Error() string {
+	return fmt.Sprintf("wrong type for configuration %s; xpected type %s",
+		err.Key, err.Type)
+}
+
+type Config struct {
+	cfg map[string]interface{}
+}
+
+func Load(r io.Reader) (Config, error) {
+	cfg := Config{map[string]interface{}{}}
+	_, err := toml.DecodeReader(r, &cfg.cfg)
+	return cfg, err
+}
+
+func (cfg Config) general() (map[string]interface{}, error) {
+	gen, ok := cfg.cfg["general"].(map[string]interface{})
+	if !ok {
+		return nil, MissingSectionError("general")
+	}
+	return gen, nil
+}
+
+func (cfg Config) Storage() (string, error) {
+	gen, err := cfg.general()
+	if err != nil {
+		return "", err
+	}
+	storage, ok := gen["storage"]
+	if !ok {
+		return "", MissingKeyError("general.storage")
+	}
+	s, ok := storage.(string)
+	if !ok {
+		return "", WrongValueTypeError{"general.storage", "string"}
+	}
+	return s, nil
+}
+
+func (cfg Config) StorageConfig() (map[string]interface{}, error) {
+	engine, err := cfg.Storage()
+	if err != nil {
+		return nil, err
+	}
+	storage, ok := cfg.cfg["storage"].(map[string]interface{})
+	if !ok {
+		return nil, MissingSectionError("storage")
+	}
+	conf, ok := storage[engine].(map[string]interface{})
+	if !ok {
+		return nil, MissingSectionError("storage." + engine)
+	}
+	return conf, nil
+}
--- cmd/tracer/main.go
+++ cmd/tracer/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/tracer/tracer"
+	"github.com/tracer/tracer/cmd/tracer/config"
 	"github.com/tracer/tracer/pb"
 	"github.com/tracer/tracer/server"
 	"github.com/tracer/tracer/storage/postgres"
@@ -13,12 +18,53 @@ import (
 	"google.golang.org/grpc"
 )
 
+func loadStorage(conf config.Config) (tracer.Storer, error) {
+	engine, err := conf.Storage()
+	if err != nil {
+		return nil, err
+	}
+	switch engine {
+	case "postgres":
+		return loadPostgres(conf)
+	default:
+		return nil, fmt.Errorf("unsupported storage engine: %s", engine)
+	}
+}
+
+func loadPostgres(conf config.Config) (tracer.Storer, error) {
+	storageConf, err := conf.StorageConfig()
+	if err != nil {
+		return nil, err
+	}
+	url, ok := storageConf["url"].(string)
+	if !ok {
+		return nil, errors.New("missing url for postgres backend")
+	}
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("drror connecting to PostgreSQL database: %s", err)
+	}
+	return postgres.New(db), nil
+}
+
 func main() {
-	db, err := sql.Open("postgres", "user=tracer dbname=postgres password=tracer sslmode=disable")
+	f, err := os.Open("example.conf")
+	if err != nil {
+		log.Fatalln("Couldn't load config:", err)
+	}
+	conf, err := config.Load(f)
+	if err != nil {
+		log.Fatalln("Couldn't load config:", err)
+	}
+	_ = f.Close()
+
+	storage, err := loadStorage(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	storage := postgres.New(db)
 	srv := &server.Server{Storer: storage}
 
 	l, err := net.Listen("tcp", ":9999")
