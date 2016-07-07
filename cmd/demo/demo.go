4bd3b887b5ebc450f9278b74d28694517a1f7414
Dominik Honnef <dominik@honnef.co>
2016-07-07 08:44:30 +0200

    Use OT helpers in demo

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -21,13 +21,12 @@ func main() {
 	t1 := tracer.NewTracer("frontend", storage, tracer.RandomID{})
 	t2 := tracer.NewTracer("backend", storage, tracer.RandomID{})
 
-	s1 := t1.StartSpan("frontend")
-	s1.SetTag(string(ext.SpanKind), "server")
+	s1 := t1.StartSpan("frontend", ext.RPCServerOption(nil))
 	s1.SetTag(string(ext.HTTPUrl), "/hello")
 	s1.SetTag(string(ext.HTTPMethod), "GET")
 
 	s2 := t1.StartSpan("backend.hello", opentracing.ChildOf(s1.Context()))
-	s2.SetTag(string(ext.SpanKind), "client")
+	s2.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
 	s2.SetTag(string(ext.Component), "grpc")
 	carrier := opentracing.TextMapCarrier{}
 	if err := t1.Inject(s2.Context(), opentracing.TextMap, carrier); err != nil {
@@ -38,12 +37,11 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	s3 := t2.StartSpan("backend.hello", opentracing.ChildOf(c3))
-	s3.SetTag(string(ext.SpanKind), "server")
+	s3 := t2.StartSpan("backend.hello", ext.RPCServerOption(c3))
 	s3.SetTag(string(ext.Component), "grpc")
 
 	s4 := t2.StartSpan("mysql", opentracing.ChildOf(s3.Context()))
-	s4.SetTag(string(ext.SpanKind), "client")
+	s4.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
 	s4.SetTag(string(ext.Component), "mysql")
 	s4.SetTag("sql.query", "SELECT * FROM table1")
 	// The MySQL server is not instrumented, so we only get the client
@@ -51,7 +49,7 @@ func main() {
 	s4.Finish()
 
 	s5 := t2.StartSpan("redis", opentracing.ChildOf(s3.Context()))
-	s5.SetTag(string(ext.SpanKind), "client")
+	s5.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
 	s4.SetTag(string(ext.Component), "redis")
 	// The Redis server is not instrumented, so we only get the client
 	// span.
