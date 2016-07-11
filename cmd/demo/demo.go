6984a5b40c82b1e8bb434b8add80e01f38ef0201
Dominik Honnef <dominik@honnef.co>
2016-07-11 03:44:36 +0200

    Update demo to latest opentracing-go version

--- cmd/demo/demo.go
+++ cmd/demo/demo.go
@@ -25,12 +25,12 @@ func main() {
 	t2 := tracer.NewTracer("backend", storage, tracer.RandomID{})
 
 	s1 := t1.StartSpan("frontend", ext.RPCServerOption(nil))
-	s1.SetTag(string(ext.HTTPUrl), "/hello")
-	s1.SetTag(string(ext.HTTPMethod), "GET")
+	ext.HTTPUrl.Set(s1, "/hello")
+	ext.HTTPMethod.Set(s1, "GET")
 
 	s2 := t1.StartSpan("backend.hello", opentracing.ChildOf(s1.Context()))
-	s2.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
-	s2.SetTag(string(ext.Component), "grpc")
+	ext.SpanKindRPCClient.Set(s2)
+	ext.Component.Set(s2, "grpc")
 	carrier := opentracing.TextMapCarrier{}
 	if err := t1.Inject(s2.Context(), opentracing.TextMap, carrier); err != nil {
 		log.Println(err)
@@ -41,19 +41,19 @@ func main() {
 		log.Println(err)
 	}
 	s3 := t2.StartSpan("backend.hello", ext.RPCServerOption(c3))
-	s3.SetTag(string(ext.Component), "grpc")
+	ext.Component.Set(s3, "grpc")
 
 	s4 := t2.StartSpan("mysql", opentracing.ChildOf(s3.Context()))
-	s4.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
-	s4.SetTag(string(ext.Component), "mysql")
+	ext.SpanKindRPCClient.Set(s4)
+	ext.Component.Set(s4, "mysql")
 	s4.SetTag("sql.query", "SELECT * FROM table1")
 	// The MySQL server is not instrumented, so we only get the client
 	// span.
 	s4.Finish()
 
 	s5 := t2.StartSpan("redis", opentracing.ChildOf(s3.Context()))
-	s5.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
-	s4.SetTag(string(ext.Component), "redis")
+	ext.SpanKindRPCClient.Set(s5)
+	ext.Component.Set(s5, "redis")
 	// The Redis server is not instrumented, so we only get the client
 	// span.
 	s5.Finish()
@@ -62,8 +62,8 @@ func main() {
 	s2.Finish()
 
 	s6 := t1.StartSpan("backend.ads", opentracing.ChildOf(s1.Context()))
-	s6.SetTag(string(ext.SpanKind), ext.SpanKindRPCClient)
-	s6.SetTag(string(ext.Component), "grpc")
+	ext.SpanKindRPCClient.Set(s6)
+	ext.Component.Set(s6, "grpc")
 	carrier = opentracing.TextMapCarrier{}
 	if err := t1.Inject(s6.Context(), opentracing.TextMap, carrier); err != nil {
 		log.Println(err)
@@ -74,11 +74,11 @@ func main() {
 		log.Println(err)
 	}
 	s7 := t2.StartSpan("backend.ads", ext.RPCServerOption(c7))
-	s7.SetTag(string(ext.Component), "grpc")
+	ext.Component.Set(s7, "grpc")
 	s7.Finish()
 	s6.Finish()
 
-	s1.SetTag(string(ext.HTTPStatusCode), 200)
+	ext.HTTPStatusCode.Set(s1, 200)
 	s1.Finish()
 
 	// Wait for spans to be flushed. Production code wouldn't need
